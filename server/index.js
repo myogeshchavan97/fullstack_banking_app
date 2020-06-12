5e03e0991fa0c62e5fab9d154635bf4a6a6345ab
Yogesh <myogeshchavan97@gmail.com>
2020-06-12 10:55:00 +0530

    changes for removing cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const authRoute = require('./routes/auth');
 const profileRoute = require('./routes/profile');
 const accountRoute = require('./routes/account');
@@ -11,7 +10,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.use(cors());
 app.use(authRoute);
 app.use(accountRoute.Router);
 app.use(profileRoute);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = 'http://localhost:5000';
+export const BASE_API_URL = '';
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
 export const GET_ERRORS = 'GET_ERRORS';
