3741a8da81c5870f18fb00e5ea4ca68e60fb99a2
Yogesh <myogeshchavan97@gmail.com>
2020-06-12 10:25:14 +0530

    changes for removing cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
+const path = require('path');
 const express = require('express');
-const cors = require('cors');
 const authRoute = require('./routes/auth');
 const profileRoute = require('./routes/profile');
 const accountRoute = require('./routes/account');
@@ -9,9 +9,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.use(cors());
 app.use(authRoute);
 app.use(accountRoute.Router);
 app.use(profileRoute);
